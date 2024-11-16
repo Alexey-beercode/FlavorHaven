ca80e4acbf776c2fc048bb6195ed09b86afeb4dd
KiSSel3 <andreyk042004@gmail.com>
2024-11-16 23:00:05 +0300

    Added CartDTO

---
+++ Backend/FlavorHaven/FlavorHaven.Application/Models/DTOs/CartDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FlavorHaven.Application.Models.DTOs;
+
+public class CartDTO
+{
+    public Guid Id { get; set; }
+    public int Count { get; set; }
+    
+    public DishDTO Dish { get; set; }
+}
