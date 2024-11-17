6929ab421f254374ef27ebbd4453ecefd4b792cb
KiSSel3 <andreyk042004@gmail.com>
2024-11-17 17:26:33 +0300

    Configurated application layer

---
+++ Backend/FlavorHaven/FlavorHaven.Application/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using FlavorHaven.Application.Models.Options;
+using FlavorHaven.Application.Providers.Implementation;
+using FlavorHaven.Application.Providers.Interfaces;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlavorHaven.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<TokenOptions>(configuration.GetSection(TokenOptions.DefaultSection));
+        
+        services.AddScoped<ITokenProvider, TokenProvider>();
+        services.AddScoped<IPasswordProvider, PasswordProvider>();
+        
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+        
+        return services;
+    }
+}
