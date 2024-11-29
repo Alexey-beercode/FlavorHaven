16d42603afcdae7739101afcafd75c35c88d0826
KiSSel3 <andreyk042004@gmail.com>
2024-11-29 14:08:00 +0300

    Created order item profile

---
+++ Backend/FlavorHaven/FlavorHaven.Application/Infrastructure/Mappers/OrderItemProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FlavorHaven.Application.Models.DTOs;
+using FlavorHaven.Domain.Entities;
+
+namespace FlavorHaven.Application.Infrastructure.Mappers;
+
+public class OrderItemProfile : Profile
+{
+    public OrderItemProfile()
+    {
+        CreateMap<OrderItems, OrderItemDTO>();
+        CreateMap<OrderItemDTO, OrderItems>();
+    }
+}
